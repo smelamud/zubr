e61fa8d01c320d952bd888e713240cf210b9e440
Shmuel Melamud <balu@migdal.ru>
2006-11-20 00:52:42 +0000

    Новое выравнивание
    git-svn-id: file:///srv/svn/zubr/trunk@9 684a2630-a932-465a-9c51-9a75a7aa8473

--- ExamScreen.py
+++ ExamScreen.py
@@ -11,42 +11,37 @@ from Screen import Screen
 class ExamScreen(Screen):
 
     def __init__(self, window):
-	Screen.__init__(self, window, gtk.Table(4, 2, True))
-
-	self.container.set_row_spacings(10)
-	self.container.set_col_spacings(10)
-
-	label = gtk.Label(u'Вопрос')
-	self.container.attach(self._boxed(label), 0, 1, 0, 1)
-
-	label = gtk.Label(u'Верно')
-	label.modify_fg(gtk.STATE_NORMAL, gtk.gdk.Color(0, 32767, 0))
-	label.modify_font(pango.FontDescription('bold'))
-	self.container.attach(self._boxed(label), 0, 1, 1, 2)
-
-	entry = gtk.Entry()
-	self.container.attach(entry, 0, 2, 2, 3)
+	Screen.__init__(self, window, gtk.VBox(False, 20))
 
 	label = gtk.Label()
 	label.set_markup(u'<span foreground="#00CC00">0</span>/' \
-	    u'<span foreground="#FF0000">2</span>/3\n23 сек')
-	label.set_justify(gtk.JUSTIFY_RIGHT)
-	label.set_line_wrap(True)
-	self.container.attach(self._boxed(label, right = True), 1, 2, 0, 2)
+	    u'<span foreground="#FF0000">2</span>/3 23 сек')
+	label.set_alignment(0.5, 0)
+	self.container.pack_start(label, False)
+
+	label = gtk.Label(u'Вопрос')
+	label.set_alignment(0, 1)
+	self.container.pack_start(label)
+
+	label = gtk.Label(u'Верно')
+	label.set_alignment(0, 0)
+	label.modify_fg(gtk.STATE_NORMAL, gtk.gdk.Color(0, 32767, 0))
+	label.modify_font(pango.FontDescription('bold'))
+	self.container.pack_start(label, False)
+
+	hbox = gtk.HBox(False, 5)
+	self.container.pack_start(hbox, False)
+
+	entry = gtk.Entry()
+	hbox.pack_start(entry)
+
+	button = gtk.Button(stock = gtk.STOCK_OK)
+	hbox.pack_start(button, False)
+
+	bbox = gtk.HButtonBox()
+	bbox.set_layout(gtk.BUTTONBOX_END)
+	self.container.pack_start(bbox, False)
 
 	button = gtk.Button(stock = gtk.STOCK_CLOSE)
 	button.connect('clicked', self.window.destroy)
-	box = gtk.HButtonBox()
-	box.set_layout(gtk.BUTTONBOX_END)
-	box.pack_start(button)
-	self.container.attach(box, 0, 2, 3, 4)
-
-    def _boxed(self, widget, right = False):
-	vbox = gtk.VBox(False)
-	vbox.pack_start(widget, False, False)
-	hbox = gtk.HBox(False)
-	if not right:
-	    hbox.pack_start(vbox, False, False)
-	else:
-	    hbox.pack_end(vbox, False, False)
-	return hbox
+	bbox.pack_start(button)
--- StartScreen.py
+++ StartScreen.py
@@ -4,19 +4,31 @@
 import pygtk
 pygtk.require('2.0')
 import gtk
+import pango
 
 from Screen import Screen
 
 class StartScreen(Screen):
 
     def __init__(self, window):
-	Screen.__init__(self, window, gtk.HBox(False))
+	Screen.__init__(self, window, gtk.VBox(False))
 
-	vbox = gtk.VBox(False)
-	self.container.pack_start(vbox, True, False)
+	label = gtk.Label(u'Начнем-с...')
+	label.modify_font(pango.FontDescription('bold'))
+	self.container.pack_start(label, True, False)
+
+	bbox = gtk.HButtonBox()
+	bbox.set_spacing(5)
+	bbox.set_layout(gtk.BUTTONBOX_END)
+	self.container.pack_start(bbox, False)
+
 	button = gtk.Button(stock = gtk.STOCK_GO_FORWARD)
 	button.connect('clicked', self.start)
-	vbox.pack_start(button, True, False)
+	bbox.pack_start(button)
+
+	button = gtk.Button(stock = gtk.STOCK_CLOSE)
+	button.connect('clicked', self.window.destroy)
+	bbox.pack_start(button)
 
     def start(self, widget, data = None):
 	self.window.switchScreen('exam')
