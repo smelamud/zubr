91a1d8468f6e5c64daca33b7f05c11b194aa4e88
Shmuel Melamud <balu@migdal.ru>
2007-02-06 18:33:40 +0000

    Попытка сделать список уроков
    git-svn-id: file:///srv/svn/zubr/trunk@21 684a2630-a932-465a-9c51-9a75a7aa8473

--- StartScreen.py
+++ StartScreen.py
@@ -4,18 +4,21 @@
 import pygtk
 pygtk.require('2.0')
 import gtk
+import gobject
 import pango
+from xml.dom.minidom import parse
+from xml import xpath
 
 from Screen import Screen
 
 class StartScreen(Screen):
 
     def __init__(self, window):
-	Screen.__init__(self, window, gtk.VBox(False))
+	Screen.__init__(self, window, gtk.VBox(False, spacing = 5))
 	self.examiner = window.examiner
 
 	fbox = gtk.HBox(False)
-	self.container.pack_start(fbox, True, False)
+	self.container.pack_start(fbox, False)
 
 	label = gtk.Label(u'Файл с вопросами')
 	fbox.pack_start(label, False, padding = 5)
@@ -26,8 +29,11 @@ class StartScreen(Screen):
 	filter.add_pattern('*.exam')
 	self.file.add_filter(filter)
 	self.file.connect('selection-changed', self.fileChanged)
-
 	fbox.pack_start(self.file, True, True)
+
+	self.listStore = gtk.ListStore(gobject.TYPE_STRING)
+	self.listView = gtk.TreeView(self.listStore)
+	self.container.pack_start(self.listView, True, True)
 
 	bbox = gtk.HButtonBox()
 	bbox.set_spacing(5)
@@ -49,6 +55,10 @@ class StartScreen(Screen):
     def fileChanged(self, filechooser = None):
 	filename = self.file.get_filename()
 	if filename != None:
+	    tree = parse(filename)
+	    lessons = xpath.Evaluate('//lesson/@title', tree)
+	    for lesson in lessons:
+		self.listStore.append((lesson.nodeValue, ))
 	    self.playButton.set_sensitive(True)
 	    self.examiner.load(filename, u'Войны')
 	else:
